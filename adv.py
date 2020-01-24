ee1360a4c9df270c3c49084643f1179f028c7a27
Michael Harris <mhguitar1997@gmail.com>
2020-01-24 08:29:21 -0800

    Creating graph

--- adv.py
+++ adv.py
@@ -34,7 +34,7 @@ map_file = "maps/test_line.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -49,6 +49,7 @@ traversal_path = []
 class Graph:
     def __init__(self):
         self.rooms = {}
+        self.length = 0
 
     def __str__(self):
         return f"Current rooms are {self.rooms}"
@@ -59,6 +60,7 @@ class Graph:
             room_copy = dict.copy(self.rooms[room_id])
             room_copy[i] = "?"
             self.rooms[room_id] = room_copy
+        self.length += 1
 
     
     def update_rooms(self, room_id, direction, links_to):
@@ -87,30 +89,52 @@ visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
 
-
+visited = {}
 traversal_graph = Graph()
 current = player.current_room
 stack = Stack()
-for room_exit in player.current_room.get_exits():
-    traversal_graph.add_room_to_graph(current.id, current.get_exits())
-    print(f"Exits {room_exit}")
-    player.travel(room_exit)
-    traversal_graph.add_room_to_graph(player.current_room.id, player.current_room.get_exits())
-    print(f"Player {player.current_room.id}")
-    print(f"Current {current.id}")
-    traversal_graph.update_rooms(current.id, room_exit, player.current_room.id)
-    # stack.push(room_exits.pop())
+
+print(room_graph)
+
+while len(visited_rooms) < len(room_graph):
+    current_room = player.current_room.id
+    room_exits = player.current_room.get_exits()
+    stack.push(room_exits)
+
+    #Check
+    if player.current_room.id not in visited_rooms:
+        # ?
+        # visited_rooms[player.current_room.id] = player.current_room.get_exits()
+        visited[player.current_room.id] = player.current_room.get_exits()
+        # for e in player.current_room.get_exits()
+
+    # print(f"While {room_exits}")
+    # push to stack then move 
+    while len(stack.stack) < 1:
+        print(stack.pop())
+
+
+
+# for room_exit in player.current_room.get_exits():
+#     traversal_graph.add_room_to_graph(current.id, current.get_exits())
+#     print(f"Exits {room_exit}")
+#     player.travel(room_exit)
+#     traversal_graph.add_room_to_graph(player.current_room.id, player.current_room.get_exits())
+#     print(f"Player {player.current_room.id}")
+#     print(f"Current {current.id}")
+#     traversal_graph.update_rooms(current.id, room_exit, player.current_room.id)
+#     # stack.push(room_exits.pop())
     # print(f"Stack - {stack}")
 
 
 # traversal_graph.add_room_to_graph(player.current_room.id, player.current_room.get_exits())
 # traversal_graph.add_room_to_graph(3)
 # traversal_graph.update_rooms(3, "n", 0)
-print(f"My Graph {traversal_graph}")
-my_queue = []
+# print(f"My Graph {traversal_graph}")
+# my_queue = []
 
 
-player.current_room.get_exits()
+# player.current_room.get_exits()
 
 
 for move in traversal_path:
